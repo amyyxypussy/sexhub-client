c94ae7e0aba70ec87f1dc16fd15a1711e6e30396
Yuanshenliu <ayuanshen.liu@gmail.com>
2024-06-25 11:23:43 +0800

    :zap: add @vueuse/core

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -47,7 +47,7 @@ export default defineConfig((env) => {
           filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
           globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
         },
-        imports: ['vue', 'vue-router']
+        imports: ['vue', 'vue-router', '@vueuse/core']
       })
     ],
     resolve: {
