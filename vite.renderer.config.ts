f9b0e90bb90b0e5f0559e2f19f251476fb35e463
Yuanshenliu <ayuanshen.liu@gmail.com>
2024-06-25 11:15:51 +0800

    :sparkles: add vue plugin and prettier

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,33 +1,36 @@
-import type { ConfigEnv, UserConfig } from "vite";
-import { defineConfig } from "vite";
-import { pluginExposeRenderer } from "./vite.base.config";
-import path from "node:path";
+import type { ConfigEnv, UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import { pluginExposeRenderer } from './vite.base.config'
+import path from 'node:path'
+
+import vue from '@vitejs/plugin-vue'
+import vueJsxPlugin from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
-  const forgeEnv = env as ConfigEnv<"renderer">;
-  const { root, mode, forgeConfigSelf } = forgeEnv;
-  const name = forgeConfigSelf.name ?? "";
+  const forgeEnv = env as ConfigEnv<'renderer'>
+  const { root, mode, forgeConfigSelf } = forgeEnv
+  const name = forgeConfigSelf.name ?? ''
 
   return {
     root,
     mode,
-    base: "./",
+    base: './',
     build: {
       outDir: `.vite/renderer/${name}`,
       rollupOptions: {
         input: {
-          index: path.resolve(__dirname, "html", "index.html"),
-          login: path.resolve(__dirname, "html", "login.html"),
-          dialog: path.resolve(__dirname, "html", "dialog.html"),
-          floatWindow: path.resolve(__dirname, "html", "floating-window.html"),
-        },
-      },
+          index: path.resolve(__dirname, 'html', 'index.html'),
+          login: path.resolve(__dirname, 'html', 'login.html'),
+          dialog: path.resolve(__dirname, 'html', 'dialog.html'),
+          floatWindow: path.resolve(__dirname, 'html', 'floating-window.html')
+        }
+      }
     },
-    plugins: [pluginExposeRenderer(name)],
+    plugins: [pluginExposeRenderer(name), vueJsxPlugin(), vue()],
     resolve: {
-      preserveSymlinks: true,
+      preserveSymlinks: true
     },
-    clearScreen: false,
-  } as UserConfig;
-});
+    clearScreen: false
+  } as UserConfig
+})
