59117d65570fc247c2e5131bbc9efe5a946063cc
amyyxypussy <707978562@qq.com>
2024-06-25 08:41:54 +0800

    :sparkles: add page entry

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,19 +1,27 @@
-import type { ConfigEnv, UserConfig } from 'vite';
-import { defineConfig } from 'vite';
-import { pluginExposeRenderer } from './vite.base.config';
+import type { ConfigEnv, UserConfig } from "vite";
+import { defineConfig } from "vite";
+import { pluginExposeRenderer } from "./vite.base.config";
+import path from "node:path";
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
-  const forgeEnv = env as ConfigEnv<'renderer'>;
+  const forgeEnv = env as ConfigEnv<"renderer">;
   const { root, mode, forgeConfigSelf } = forgeEnv;
-  const name = forgeConfigSelf.name ?? '';
+  const name = forgeConfigSelf.name ?? "";
 
   return {
     root,
     mode,
-    base: './',
+    base: "./",
     build: {
       outDir: `.vite/renderer/${name}`,
+      rollupOptions: {
+        input: {
+          index: path.resolve(__dirname, "html", "index.html"),
+          login: path.resolve(__dirname, "html", "login.html"),
+          dialog: path.resolve(__dirname, "html", "dialog.html"),
+        },
+      },
     },
     plugins: [pluginExposeRenderer(name)],
     resolve: {
